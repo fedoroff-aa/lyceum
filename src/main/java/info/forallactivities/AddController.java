d4bd3b3a3bbaee50d436b34bee716b2868d416b6
AlekseyFedorov <AlekseyFedorov@main>
2018-08-05 01:04:47 +0300

    4

--- src/main/java/info/forallactivities/AddController.java
+++ src/main/java/info/forallactivities/AddController.java
@@ -1,5 +1,18 @@
 package info.forallactivities;
 
-public class AddController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
+public class AddController {
+	@RequestMapping("/add")
+	public ModelAndView add(@RequestParam("f1") String s) {
+		System.out.println("Hello from servlet");
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("success.jsp");
+		mav.addObject("f1", s);
+		return mav;
+	}
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,8 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+	<form action="add">
+		<input type="text" name="f1"/><br/>
+		<input type="submit"/>
+	</form>
 </body>
 </html>
