b6eed08fd668f9bb7e6075d09dced948f3ed1add
AlekseyFedorov <AlekseyFedorov@main>
2018-08-05 18:15:53 +0300

    9

--- src/main/java/info/forallactivities/AddController.java
+++ src/main/java/info/forallactivities/AddController.java
@@ -1,5 +1,10 @@
 package info.forallactivities;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +15,17 @@ public class AddController {
 	@RequestMapping("/add")
 	public ModelAndView add(@RequestParam("f1") String s) {
 		System.out.println("Hello from servlet!!!");
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://mainapp-mysql:3306/workspace", "userhKP", "ILweMo3x");
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery("select * from people;");
+			while (rs.next()) {for (int i = 1; i <= 2; i++) System.out.print(rs.getString(i) + " "); System.out.println();}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("success.jsp");
 		mav.addObject("f1", s);
